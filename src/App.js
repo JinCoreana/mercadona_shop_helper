d0f5c8df39893484cdcf837c35197d0c4e65dca1
Seoyeonjin Choi <itmustbejin@gmail.com>
2022-06-08 12:08:24 +0200

    220608

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AddItem from './AddItem';
 import Contents from './Contents';
 import Footer from './Footer';
 import Header from './Header';
 import SearchItem from './SearchItem';
+import apiRequest from './apiRequest';
 
 function App() {
+  const API_URL ='http://localhost:3500/items'
+  const [items, setItems] = useState([])
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')))
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 //     {
 //         id:1,
 //         checked: false,
@@ -24,20 +28,46 @@ function App() {
 //         checked: false,
 //         item: 'Brush'
 //     },
+
 // 
-
-const setAndSaveItems= (newItems) => {
-  setItems(newItems)
-  localStorage.setItem(JSON.stringify(newItems))
+useEffect(()=>{
+const fetchItems=async()=>{
+  try{
+    const response = await fetch(API_URL)
+    if(!response.ok) throw Error('Did not receive expected data')
+    const listItems = await response.json()
+    console.log(listItems)
+    setItems(listItems)
+    setFetchError(null)
+  }
+  catch (err){
+    setFetchError(err.message)
+  }
+  finally{
+    setIsLoading(false)
+  }
 }
+(async ()=> await fetchItems())()
+},[])
 
-const addItem=(item)=>{
+
+const addItem=async(item)=>{
  const id = items.length? items[items.length-1].id+1 : 1
  const newItem = {id, checked:false, item}
  const listItems= [...items, newItem]
- setAndSaveItems(listItems)
+ setItems(listItems)
 
+const postOptions={
+method:'POST',
+headers:{
+  'Content-Type':'application/json'
+},
+body: JSON.stringify(newItem)
 }
+const result= await apiRequest(API_URL, postOptions)
+if(result) setFetchError(result)
+}
+
 const handleSubmit= (e)=>{
   e.preventDefault()
   if(!newItem) return
@@ -45,17 +75,36 @@ const handleSubmit= (e)=>{
   setNewItem('');
 }
 
-const onChange = (id)=> {
+const onChange = async(id)=> {
   const updatedList = items.map((item)=> item.id === id? {...item,
-  checked: !item.checked}: item)
-  setAndSaveItems(updatedList)
+checked: !item.checked}: item)
+  setItems(updatedList)
 //이렇게 저장해놓으면 다시 로드했을때 마지막으로 저장된 값 불러와짐.
+
+const clickedItem = updatedList.filter((item)=> item.id===id)
+console.log(clickedItem[0].checked)
+const patchOptions={
+  method:'PATCH',
+  headers:{
+    'Content-Type':'application/json'
+  },
+  body: JSON.stringify({checked: clickedItem[0].checked})
+}
+const reqUrl = `${API_URL}/${id}`
+const result= await apiRequest(reqUrl, patchOptions)
+if(result) setFetchError(result)
 }
 
-const handleDelete= (id) =>{  
+const handleDelete= async(id) =>{  
   const updatedList = items.filter((item)=> item.id !== id)
-   setAndSaveItems(updatedList)
-}
+   setItems(updatedList)
+   const deleteOptions={
+     method:'DELETE' }
+     const reqUrl = `${API_URL}/${id}`
+     const result = await apiRequest(reqUrl, deleteOptions)
+     if(result) setFetchError(result)
+   }
+
 
   return (
    <div className='App'>
@@ -69,12 +118,16 @@ const handleDelete= (id) =>{
      search={search}
      setSearch={setSearch}
      />
-      <Contents 
+     <main>
+       {isLoading && <p>Loading Items...</p>}
+       {fetchError && <p style={{color:'red'}}>{`Error:${fetchError}`}</p>}
+      {!fetchError && !isLoading && <Contents 
       items={items.filter(items => ((items.item).toLowerCase()).includes
       (search.toLowerCase()))}
       onChange={onChange}
       handleDelete={handleDelete}
-      />
+      />}
+      </main>
       <Footer number={items.length}/>
    </div>
   );
